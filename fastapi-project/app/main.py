5c247ea05a81df27d1f31b449fe3d845a0e82d85
KummariJohnson <johnsonkummari@gmail.com>
2025-07-04 15:07:29 +0530

    Delete fastapi-project directory

--- fastapi-project/app/main.py
+++
@@ -1,7 +0,0 @@
-# app/main.py
-from fastapi import FastAPI
-from app.routes import router
-
-app = FastAPI(title="FastAPI + MongoDB CRUD")
-
-app.include_router(router)
--- fastapi-project/app/models.py
+++
@@ -1,9 +0,0 @@
-# app/models.py
-from pydantic import BaseModel, EmailStr
-
-class User(BaseModel):
-    name: str
-    email: EmailStr
-
-class UserResponse(User):
-    id: str
